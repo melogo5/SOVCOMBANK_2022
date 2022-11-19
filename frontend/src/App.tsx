ce65c6a46b6776340f533e5e9886cc26070f72cb
Andrei Potashin <a.potashin@pnlfin.tech>
2022-11-19 11:31:15 +0200

    Process review

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,15 @@ import Login from './pages/Login/Login';
 
 import './App.css';
 import Registration from './pages/Registration/Registration';
+import { ProcessReview } from './pages/ProcessReview/ProcessReview';
 
 const App: FC = () => {
 
   return (
     <div className="App">
       {/* <Login /> */}
-      <Registration />
+      {/* <Registration /> */}
+      <ProcessReview />
     </div>
   );
 }
